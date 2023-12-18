540fbf39f65db4bd4ba9350635a531fb6a72422e
Pedro Gimenez <gimenezcavalcantepedro@gmail.com>
2023-12-18 12:32:39 -0300

    "Add new entities."

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,4 +1,5 @@
 import { Answer } from "../entities/answer"
+import { AsnwerRepository } from "../repositories/answers-repository"
 
 interface AnswerQuestionUseCaseRequest {
   instructorId: string
@@ -7,8 +8,17 @@ interface AnswerQuestionUseCaseRequest {
 }
 
 export class AnswerQuestionUseCase {
-  execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
-    const answer = new Answer(content)
+  constructor(private answerRepository: AsnwerRepository) {}
+
+  async execute({
+    instructorId,
+    questionId,
+    content,
+  }: AnswerQuestionUseCaseRequest) {
+    const answer = new Answer({ authorId: instructorId, content, questionId })
+
+    await this.answerRepository.create(answer)
+
     return answer
   }
 }
