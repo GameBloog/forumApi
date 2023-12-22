cfa137c55e3554349eb7cb699ec3513969f2a2f7
Pedro Gimenez <gimenezcavalcantepedro@gmail.com>
2023-12-22 14:35:39 -0300

    change some props

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,10 +1,14 @@
 import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 
 interface QuestionProps {
+  authorId: UniqueEntityID
+  bestAnswerId?: UniqueEntityID
   title: string
   content: string
-  authorId: string
   slug: Slug
+  createdAt: Date
+  updatdeAt?: Date
 }
 export class Question extends Entity<QuestionProps> {}
