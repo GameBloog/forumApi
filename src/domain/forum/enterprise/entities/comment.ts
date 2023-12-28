579f8b6e0a1d03edf669ac8fdbfa760db3d4b857
Pedro Gimenez <gimenezcavalcantepedro@gmail.com>
2023-12-28 12:36:31 -0300

    base class comment

---
+++ src/domain/forum/enterprise/entities/comment.ts
@@ -0,0 +1,36 @@
+import { Entity } from "@/core/entities/entity"
+import { UniqueEntityID } from "@/core/entities/unique-entity-id"
+import { extend } from "dayjs"
+
+export interface CommentProps {
+  authorId: UniqueEntityID
+  content: string
+  createdAt: Date
+  updatdeAt?: Date
+}
+export abstract class Comment<Props extends CommentProps> extends Entity<Props> {
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatdeAt() {
+    return this.props.updatdeAt
+  }
+
+  private touch() {
+    this.props.updatdeAt = new Date()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
+}
--- src/domain/forum/enterprise/entities/question-comment.ts
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -1,38 +1,13 @@
-import { Entity } from "@/core/entities/entity"
 import { UniqueEntityID } from "@/core/entities/unique-entity-id"
 import { Optional } from "@/core/types/optional"
+import { Comment, CommentProps } from "./comment"
 
-export interface QuestionCommentProps {
-  authorId: UniqueEntityID
+export interface QuestionCommentProps extends CommentProps {
   questionId: UniqueEntityID
-  content: string
-  createdAt: Date
-  updatdeAt?: Date
 }
-export class QuestionComment extends Entity<QuestionCommentProps> {
-  get authorId() {
-    return this.props.authorId
-  }
-
-  get content() {
-    return this.props.content
-  }
-
-  get createdAt() {
-    return this.props.createdAt
-  }
-
-  get updatdeAt() {
-    return this.props.updatdeAt
-  }
-
-  private touch() {
-    this.props.updatdeAt = new Date()
-  }
-
-  set content(content: string) {
-    this.props.content = content
-    this.touch()
+export class QuestionComment extends Comment<QuestionCommentProps> {
+  get questionId() {
+    return this.props.questionId
   }
 
   static create(
